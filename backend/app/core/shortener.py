7e6b281cc81026fc1e0f4bd1d98bc075ee5b04ea
farhanahmed3223 <farhanahmed3223@proton.me>
2022-06-20 22:11:00 +0500

    wip - custom slugs not working yet

--- backend/app/core/shortener.py
+++ backend/app/core/shortener.py
@@ -1,6 +1,9 @@
-import random
-import string
+import random, string, re
 
 def generate_short_code(length: int = 7) -> str:
     chars = string.ascii_letters + string.digits
     return "".join(random.choices(chars, k=length))
+
+def validate_custom_slug(slug: str) -> bool:
+    # TODO: proper validation
+    return bool(slug) and len(slug) >= 3
