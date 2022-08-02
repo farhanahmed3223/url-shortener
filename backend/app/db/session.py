0c696b2a19c37d233d7f56d342f2aac310a62e29
farhanahmed3223 <farhanahmed3223@proton.me>
2022-08-02 14:55:00 +0500

    fix: async session not closing properly

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,11 +1,10 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from app.db.models import Base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432/urlshortener")
-engine = create_async_engine(DATABASE_URL)
-AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(DATABASE_URL, echo=False)
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_tables():
     async with engine.begin() as conn:
@@ -13,4 +12,7 @@ async def create_tables():
 
 async def get_db():
     async with AsyncSessionLocal() as session:
-        yield session
+        try:
+            yield session
+        finally:
+            await session.close()
