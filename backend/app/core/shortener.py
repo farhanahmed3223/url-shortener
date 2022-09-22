5c3be6613275c18068d99122cf6dce804da76e3e
farhanahmed3223 <farhanahmed3223@proton.me>
2022-09-22 19:33:00 +0500

    fix: timezone bug in expiry check

--- backend/app/core/shortener.py
+++ backend/app/core/shortener.py
@@ -1,8 +1,22 @@
-import random, string, re
+import hashlib, time, string, re
 
-def generate_short_code(length: int = 7) -> str:
-    chars = string.ascii_letters + string.digits
-    return "".join(random.choices(chars, k=length))
+BASE62_CHARS = string.ascii_letters + string.digits
+
+def _sha256_to_int(data: str) -> int:
+    digest = hashlib.sha256(data.encode()).digest()
+    return int.from_bytes(digest, byteorder="big")
+
+def _int_to_base62(n: int, length: int = 7) -> str:
+    result = []
+    while n > 0:
+        result.append(BASE62_CHARS[n % 62])
+        n //= 62
+    code = "".join(reversed(result))
+    return code[:length].ljust(length, BASE62_CHARS[0])
+
+def generate_short_code(url: str, length: int = 7) -> str:
+    seed = f"{url}{time.time_ns()}"
+    return _int_to_base62(_sha256_to_int(seed), length)
 
 def validate_custom_slug(slug: str) -> bool:
     if not slug:
