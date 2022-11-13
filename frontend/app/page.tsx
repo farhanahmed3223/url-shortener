0249690d92024d86e12b170cdb771120c8fa622f
farhanahmed3223 <farhanahmed3223@proton.me>
2022-11-13 15:22:00 +0500

    frontend: copy to clipboard button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,10 @@
+import { ShortenForm } from "@/components/ShortenForm"
+
 export default function Home() {
   return (
-    <main>
-      <h1>URL Shortener</h1>
-      <p>Coming soon</p>
+    <main className="container mx-auto py-16 px-4">
+      <h1 className="text-3xl font-bold mb-8">Snip — URL Shortener</h1>
+      <ShortenForm />
     </main>
   )
 }
